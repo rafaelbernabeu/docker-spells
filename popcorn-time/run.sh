f18a9f5adbe0a56f27327590e06cf2dddf08d0a7
Rafael Bernabeu <rbbernabeu@gmail.com>
2021-02-04 23:32:36 -0300

    atualizando imagem do popcorn com config default

--- popcorn-time/run.sh
+++ popcorn-time/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-docker run --rm -ti -e DISPLAY \
+docker run -d -e DISPLAY \
+    --name popcorn-time \
     --device=/dev/snd:/dev/snd \
+    -v ~/popcorn-time:/tmp/Popcorn-Time \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket \
     -v /dev/shm:/dev/shm \
